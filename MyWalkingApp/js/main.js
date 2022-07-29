5cb164192919d8633a18518708512748155d45e1
NOI <88365739+noiash@users.noreply.github.com>
2022-07-29 15:33:38 +0900

    [Add]アプリ紹介ページに動きをつけよう

---
+++ MyWalkingApp/js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+{
+  // Intersection Observer API
+
+  function callback(entries, obs) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      }
+
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+  const observer = new IntersectionObserver(callback, {
+    threshold: 0.2,
+  });
+
+  document.querySelectorAll('.animate').forEach(el => {
+    observer.observe(el);
+  });
+
+}
